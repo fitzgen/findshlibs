648aef24fe2dee6d4cf2e485490d7ed9d34a318f
Ivan Molodetskikh <yalterz@gmail.com>
2018-01-21 11:25:55 +0300

    Remove wrong assert
    On i686 libraries frequently get loaded at high addresses (0xf...),
    triggering this assert. Removing it makes the code work correctly.

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -156,7 +156,6 @@ impl<'a> SharedLibraryTrait for SharedLibrary<'a> {
 
     #[inline]
     fn virtual_memory_bias(&self) -> Bias {
-        assert!((self.addr as usize) < (isize::MAX as usize));
         Bias(self.addr as usize as isize)
     }
 
